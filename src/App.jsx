0760fd2c02903f81faf97eb1c568a38c877bcafb
tiwariharshitraj14 <tiwariharshitraj28@gmail.com>
2025-05-01 12:55:54 +0530

    feat: add UserDetails and DeleteAllUser components, update App to render UserDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import UserDetails from "./components/UserDetails";
 
 const App = () => {
   return (
     <>
     <NavBar/>
+    <UserDetails/>
     </>
   )
 }
---
+++ src/components/DeleteAllUser.jsx
@@ -0,0 +1,9 @@
+const DeleteAllUser = () => {
+  return (
+    <>
+      <div>DeleteAllUser</div>
+    </>
+  );
+};
+
+export default DeleteAllUser;
